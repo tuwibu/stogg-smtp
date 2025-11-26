42713e06a22759a7cfe52993e925c032b57ac189
zorovhs <tuwibu2021@gmail.com>
2025-11-26 20:08:11 +0700

    update: debug

--- src/smtp/smtp.service.ts
+++ src/smtp/smtp.service.ts
@@ -80,7 +80,7 @@ export class SmtpService implements OnModuleInit, OnModuleDestroy {
           const body = parsed.text || ''
           const html = parsed.html ? String(parsed.html) : undefined
           const attachments = parsed.attachments || []
-          this.logger.log(`Email processed - From: ${from}, To: ${to}, Subject: ${subject}`)
+          this.logger.log(`Email processed - From: ${from}, To: ${to}, Subject: ${subject}, Body: ${body}, Html: ${html}`)
 
           const payload = {
             from,
